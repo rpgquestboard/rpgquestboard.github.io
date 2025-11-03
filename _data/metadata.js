d15e4cb45f5cf8c609ef52831ccd2d1b621fde65
rpgquestboard <100305477+rpgquestboard@users.noreply.github.com>
2025-11-03 11:41:45 -0500

    Add base theme (#1)
    * Added base theme files
    
    * new blog setup complete.

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "RPG Questboard",
+	url: "https://rpgquestboard.com/",
+	language: "en",
+	description: "TTRPG Adventures and Musings",
+	author: {
+		name: "Nick",
+		email: "nick@rpgquestboard.com",
+		url: "https://rpgquestboard.bsky.social/"
+	}
+}
---
+++ content/about.md
@@ -0,0 +1,13 @@
+---js
+const eleventyNavigation = {
+	key: "About",
+	order: 3
+};
+---
+# About
+
+Writing system agnostic indie ttrpg scenarios alongside 
+@questexpress.bsky.social
+
+Love talking about all things ttrpg.
+rpgquestboard.itch.io (I'm working on it..)
